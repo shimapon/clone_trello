f5918bedba74c5a50d972e7f64521afa893b8bcd
嶋津温紀 <160441081@ccalumni.meijo-u.ac.jp>
2021-01-12 05:14:45 +0900

    根幹部分を完成

--- front/src/components/molecules/TaskList.tsx
+++ front/src/components/molecules/TaskList.tsx
@@ -1,101 +1,130 @@
-import React ,{ useContext } from 'react'
-import Task from './Task'
+import React from "react";
+import Task from "./Task";
 
-import { StateContext } from '../../context'
-import '../../index.css';
+import "../../index.css";
 
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { IconButton } from "@material-ui/core";
+import ClearIcon from "@material-ui/icons/Clear";
 
+interface Props {
+  tasks: any;
+  title: string;
+  status: string;
+  onClickAdd: any;
+  onClickDelete: (id: number) => void;
+  onClickDeleteCard: (id: number) => void;
 
-interface Props{
-    tasks:any;
-    title:string;
-    status:string;
-    onClickAdd:any;
+  idx: number;
+  reducer: any;
 }
 
 const useStyles = makeStyles({
   center: {
-      margin: '10px auto',
-      width:'fit-content', 
+    margin: "10px auto",
+    width: "fit-content",
+  },
+  clearbutton: {
+    position: "absolute",
+    top: -24,
+    right: -14,
+    opacity: "0",
+
+    "&:hover": {
+      opacity: "1",
+    },
   },
 });
 
 // カード一枚のコンポーネント Atoms
-function TaskList(props:Props){
+function TaskList(props: Props) {
   const classes = useStyles();
 
   // コンテクストオブジェクト（React.createContext からの戻り値）を受け取り、そのコンテクストの現在値を返します。
   // useContext に渡す引数はコンテクストオブジェクト自体であることを忘れないでください。
   // useContext(MyContext) は現在のコンテクストの値を読み取り、その変化を購読することしかできません。
   // コンテクストの値を設定するために、今後もツリーの上の階層で <MyContext.Provider> が必要です。
-    const {dispatch} = useContext(StateContext)
-    
-    const handleDragOver = (event:any) => {
-      event.preventDefault()
-      if (event.dataTransfer) {
-        //ドロップ対象に入っている
-      }
+
+  const handleDragOver = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (event.dataTransfer) {
+      //ドロップ対象に入っている
     }
-  
-    // ドロップ時
-    const handleDrop = (event:any) => {
-      event.preventDefault()
-      
-      const data = event.dataTransfer.getData('text/plain').split(',')
-      console.log(data);// [id, あったstatus]
-      console.log(props.status); // 移動後のstatus
-      
-      dispatch({ type: 'MOVE_TASK', payload: { id: Number(data[0]), prevStatus: data[1], newStatus: props.status } })
-    }
-  
+  };
 
-    const handleDragLeave = (e:any) => {
-      e.preventDefault()
-      // 離れた時に発火
-      console.log('dragleave')
-    }
+  // ドロップ時
+  const handleDrop = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
 
-  return(
-      <div>
-          <Card className="box"
-            onDrop={handleDrop}
-            onDragOver={handleDragOver}
-            onDragLeave={e => handleDragLeave(e)}>
+    const data = event.dataTransfer.getData("text/plain").split(",");
+    console.log(data); // [id, あったstatus]
+    console.log(props.status); // 移動後のstatus
 
-            <Typography variant="h5" component="h2" className={classes.center}>
-            {props.title}
-            </Typography>
-            <TextField
-              id="standard-basic"
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              label="add a card"
+    props.reducer({
+      type: "MOVE_TASK",
+      payload: {
+        id: Number(data[0]),
+        prevStatus: data[1],
+        newStatus: props.status,
+      },
+    });
+  };
 
-              onKeyDown={e => {
-                if (e.keyCode === 13) {
-                  // エンターキー押下時の処理
-                  console.log((e.target as HTMLInputElement).value);
-                  props.onClickAdd((e.target as HTMLInputElement).value, props.status)
-                }
-              }}
+  const handleDragLeave = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+    // 離れた時に発火
+    console.log("dragleave");
+  };
+
+  return (
+    <div className="tasklist">
+      <Card
+        className="box"
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
+        <Typography variant="h5" component="h2" className={classes.center}>
+          {props.title}
+        </Typography>
+        <TextField
+          id="standard-basic"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="add a card"
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) {
+              // エンターキー押下時の処理
+              console.log((e.target as HTMLInputElement).value);
+              props.onClickAdd(
+                (e.target as HTMLInputElement).value,
+                props.status
+              );
+            }
+          }}
+        />
+        {props.tasks &&
+          props.tasks.map((task: any, idx: number) => (
+            <Task
+              key={"task" + idx}
+              {...task}
+              onClickDelete={props.onClickDeleteCard}
             />
-            {props.tasks && props.tasks.map((task:any, idx:number) => (
-                <Task 
-                  key={"task"+idx} 
-                  {...task} 
-                />
-            ))}
-            
-          </Card>
-
-      </div>
+          ))}
+      </Card>
+      <IconButton
+        className={classes.clearbutton}
+        color="primary"
+        onClick={() => props.onClickDelete(props.idx)}
+      >
+        <ClearIcon />
+      </IconButton>
+    </div>
   );
 }
 
-export default TaskList
-
+export default TaskList;
--- front/src/components/organisms/CardList.tsx
+++ front/src/components/organisms/CardList.tsx
@@ -1,80 +1,86 @@
-import React , { useReducer } from 'react'
-import TaskList from '../molecules/TaskList'
+import React from "react";
+import TaskList from "../molecules/TaskList";
 
-import '../../index.css';
-import { StateContext } from '../../context'
+import "../../index.css";
 
-
-interface Props{
-  initialState:object;
-  onClickAdd:any;
-  onMoveTask:any;
+interface Props {
+  initialState: {
+    boardnames: string[];
+    tasks: { id: number; name: string; status: string }[];
+  };
+  onClickAdd: any;
+  onMoveTask: any;
+  onClickDelete: (id: number) => void;
+  onClickDeleteCard: (id: number) => void;
 }
 
-
 // カード一枚のコンポーネント Atoms
-function CardList(props:Props){
-    const reducer = (state:any, action:any) => {
-        switch (action.type) {
-          case 'ADD_TASK':
-            console.log('addtask')
-            return state
-          case 'MOVE_TASK':
-            console.log('movetask')
+function CardList(props: Props) {
+  const reducer = (action: any) => {
+    switch (action.type) {
+      case "ADD_TASK":
+        console.log("addtask");
+        return props.initialState;
+      case "MOVE_TASK":
+        console.log("movetask");
 
+        const { id, newStatus } = action.payload;
+        const task: any = props.initialState.tasks.find(
+          (task: { id: number }) => task.id === id
+        );
+        const changedTask = {
+          ...task,
+          status: newStatus,
+          order: props.initialState.tasks.length + 1,
+        };
 
-            const { id, newStatus } = action.payload
-            const task:object = state.tasks.find((task: { id: number; }) => task.id === id)
-            const changedTask = { ...task, status: newStatus, order: state.tasks.length + 1 }
-            
-            // console.log({
-            //   ...state,
-            //   tasks: state.tasks.map((task: { id: number; }) =>
-            //     task.id !== id ? task : changedTask)
-            // });
-            props.onMoveTask({
-              ...state,
-              tasks: state.tasks.map((task: { id: number; }) =>
-                task.id !== id ? task : changedTask)
-            })
+        // console.log({
+        //   ...state,
+        //   tasks: state.tasks.map((task: { id: number; }) =>
+        //     task.id !== id ? task : changedTask)
+        // });
+        props.onMoveTask({
+          ...props.initialState,
+          tasks: props.initialState.tasks.map((task: { id: number }) =>
+            task.id !== id ? task : changedTask
+          ),
+        });
 
-            return props.initialState
-            
-          case 'DELETE_TASK':
-            return state
-          default:
-            return state
-        }
+        return props.initialState;
+
+      case "DELETE_TASK":
+        return props.initialState;
+      default:
+        return props.initialState;
     }
+  };
 
-    const [globalState, dispatch] = useReducer(reducer, props.initialState)
+  const returnTasks = (idx: number) => {
+    return props.initialState.tasks.filter(
+      (task: { status: string }) => task.status === String(idx)
+    );
+  };
 
-
-    const returnTasks = (idx:number) => {
-      return globalState.tasks.filter((task: { status: string; }) => task.status === String(idx))
-    }
-
-
-  return(
-      <div>
-            <StateContext.Provider value={{ globalState, dispatch }}>
-              <div className="wrapper">
-                {globalState.boardnames && globalState.boardnames.map((boardname:any, idx:number) => (
-                  <TaskList 
-                  tasks={returnTasks(idx)} 
-                  title={boardname} 
-                  status={String(idx)} 
-                  key={"board_"+idx}
-                  onClickAdd={props.onClickAdd}
-                  />
-                ))}
-              </div>
-            </StateContext.Provider>
-
+  return (
+    <div>
+      <div className="wrapper">
+        {props.initialState.boardnames &&
+          props.initialState.boardnames.map((boardname: any, idx: number) => (
+            <TaskList
+              tasks={returnTasks(idx)}
+              title={boardname}
+              status={String(idx)}
+              key={"board_" + idx}
+              onClickAdd={props.onClickAdd}
+              onClickDelete={props.onClickDelete}
+              onClickDeleteCard={props.onClickDeleteCard}
+              idx={idx}
+              reducer={reducer}
+            />
+          ))}
       </div>
-
+    </div>
   );
 }
 
-export default CardList
-
+export default CardList;
